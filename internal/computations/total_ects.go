2d3e451fa18709793b699be419e7850288db4529
Radu-Cristian Sarău <radusarau2004@outlook.com>
2026-02-12 18:10:34 +0100

    Adds functions for parsing and computing ECTS data

--- internal/computations/total_ects.go
+++ internal/computations/total_ects.go
@@ -1,5 +1,25 @@
 package computations
 
+import (
+	"fmt"
+	"strconv"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+func ParseECTS(courses []bson.M) []float64 {
+	var ects []float64
+	for _, course := range courses {
+		ectsStr := fmt.Sprintf("%v", course["ECTS"])
+		val, err := strconv.ParseFloat(ectsStr, 64)
+		if err != nil {
+			continue
+		}
+		ects = append(ects, val)
+	}
+	return ects
+}
+
 func TotalECTS(ects []float64) float64 {
 	totalECTS := 0.0
 	for _, ectsValue := range ects {
