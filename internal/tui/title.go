1634321fb61b3bc339344feaffaa0283dc78d70c
Radu-Cristian Sarău <radusarau2004@outlook.com>
2026-02-13 02:36:22 +0100

    Defines title color

--- internal/tui/title.go
+++ internal/tui/title.go
@@ -18,8 +18,10 @@ const title = `
 ████████▀   ▀█   █▀  █▀     ████████▀    ███    ███   ███    █▀  ████████▀    ██████████  ▄████████▀  
 `
 
+const TitleColor = lipgloss.Color("#17b118")
+
 func RenderTitle() string {
 	width, _, _ := term.GetSize(uintptr(os.Stdout.Fd()))
-	titleStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#17b118")).Bold(true).Width(width).Align(lipgloss.Center)
+	titleStyle := lipgloss.NewStyle().Foreground(TitleColor).Bold(true).Width(width).Align(lipgloss.Center)
 	return titleStyle.Render(title)
 }
